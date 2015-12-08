a094e714dc61e151c9388cbf34d1ff1a820f4622
zoedujeux <dujeuxzoe3@msn.com>
2015-12-08 13:57:35 +0100

    fourth commit

--- src/ZD/AdminBundle/Controller/AdminController.php
+++ src/ZD/AdminBundle/Controller/AdminController.php
@@ -1,16 +1,19 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+namespace ZD\AdminBundle\Controller;
 
-/**
- * Description of AdminController
- *
- * @author zoe
- */
-class AdminController {
-    //put your code here
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class AdminController extends Controller 
+{
+     public function indexAction()
+    {
+
+          return $this->render('ZDAdminBundle:Admin:index.html.twig', array(
+            'index' => $index
+            ));
+    }
 }
