9d6a0da2261f55fcd2d5f7ea369d405ea0c6af14
zoedujeux <dujeuxzoe3@msn.com>
2015-12-13 13:25:40 +0100

    5th commit

--- src/ZD/BlogBundle/Controller/ArticleController.php
+++ src/ZD/BlogBundle/Controller/ArticleController.php
@@ -5,6 +5,8 @@ namespace ZD\BlogBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use ZD\BlogBundle\Entity\FormRegister;
+
 
 class ArticleController extends Controller
 {
@@ -37,5 +39,110 @@ class ArticleController extends Controller
           ));
     }
     
+    public function registerAction (Request $request)
+    {
+  
+        $register = new FormRegister();
+
+        
+        $form = $this->get('form.factory')->createBuilder('form', $register)
+
+          ->add('name',      'text')
+          ->add('email',     'email')
+          ->add('password',   'password')
+          ->add('save',      'submit')
+          ->getForm()
+        ;
+
+        $form = handleRequest($request);
+        
+        if ($form->isValid()) {
+       
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($register);
+        $em->flush();
+
+        $request->getSession()->getFlashBag()->add('notice', 'Vous avez bien été enregistré.');
+
+       
+        return $this->redirect($this->generateUrl('zd_blog_register', array('id' => $register->getId())));
+      }
+
+      
+     
+      return $this->render('ZDBlogBundle:Article:register.html.twig', array(
+        'form' => $form->createView(),
+      ));
+
+    
+    }
+    
+     public function loginAction (Request $request)
+    {
+
+    }
+    
+     public function addAction(Request $request)
+    {
+//            $advert = new Advert();
+//            $form = $this->createForm(new AdvertType(), $advert);
+//
+//            if ($form->handleRequest($request)->isValid()) {    
+//              $em = $this->getDoctrine()->getManager();
+//              $em->persist($advert);
+//              $em->flush();
+//
+//              $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+//
+//              return $this->redirect($this->generateUrl('oc_platform_view', array('id' => $advert->getId())));
+//            }
+//
+//            return $this->render('OCPlatformBundle:Advert:add.html.twig', array(
+//              'form' => $form->createView(),
+//              'advert' => $advert
+//            ));
+
+    }
+    
+    
+    //**********************************************************
+    //****EDIT*************************************************
+    //**********************************************************
+    
+    public function editAction($id, Request $request)
+    {
+
+    }
+    
+    
+    //**********************************************************
+    //****DELETE*************************************************
+    //**********************************************************
+    public function deleteAction($id, Request $request)
+    {
+      
+    }
+    
+    //**********************************************************
+    //***MENU*************************************************
+    //**********************************************************
+    
+//    public function menuAction($limit = 3)
+//    {
+//      $listAdverts = $this->getDoctrine()
+//        ->getManager()
+//        ->getRepository('OCPlatformBundle:Advert')
+//        ->findBy(
+//          array(),                 // Pas de critère
+//          array('date' => 'desc'), // On trie par date décroissante
+//          $limit,                  // On sélectionne $limit annonces
+//          0                        // À partir du premier
+//      );
+//
+//      return $this->render('OCPlatformBundle:Advert:menu.html.twig', array(
+//        'listAdverts' => $listAdverts
+//      ));
+//    }
+//    
     
 }
---
+++ src/ZD/BlogBundle/Entity/FormRegister.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace ZD\BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * FormRegister
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="ZD\BlogBundle\Entity\FormRegisterRepository")
+ */
+class FormRegister
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return FormRegister
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return FormRegister
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return FormRegister
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+}
+
