a564118919ed810bed823d1c24ba5403bc107a5d
zoedujeux <dujeuxzoe3@msn.com>
2016-01-21 18:16:30 +0100

    commit-21-01

--- src/ZD/BlogBundle/Controller/ArticleController.php
+++ src/ZD/BlogBundle/Controller/ArticleController.php
@@ -61,67 +61,10 @@ class ArticleController extends Controller
     
    
     
-     public function addAction(Request $request)
-    {
-//            $advert = new Advert();
-//            $form = $this->createForm(new AdvertType(), $advert);
-//
-//            if ($form->handleRequest($request)->isValid()) {    
-//              $em = $this->getDoctrine()->getManager();
-//              $em->persist($advert);
-//              $em->flush();
-//
-//              $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
-//
-//              return $this->redirect($this->generateUrl('oc_platform_view', array('id' => $advert->getId())));
-//            }
-//
-//            return $this->render('OCPlatformBundle:Advert:add.html.twig', array(
-//              'form' => $form->createView(),
-//              'advert' => $advert
-//            ));
-
-    }
-    
-    
-    //**********************************************************
-    //****EDIT*************************************************
-    //**********************************************************
-    
-    public function editAction($id, Request $request)
-    {
-
-    }
-    
-    
-    //**********************************************************
-    //****DELETE*************************************************
-    //**********************************************************
-    public function deleteAction($id, Request $request)
-    {
-      
-    }
-    
-    //**********************************************************
-    //***MENU*************************************************
-    //**********************************************************
-    
-//    public function menuAction($limit = 3)
-//    {
-//      $listAdverts = $this->getDoctrine()
-//        ->getManager()
-//        ->getRepository('OCPlatformBundle:Advert')
-//        ->findBy(
-//          array(),                 // Pas de critère
-//          array('date' => 'desc'), // On trie par date décroissante
-//          $limit,                  // On sélectionne $limit annonces
-//          0                        // À partir du premier
-//      );
-//
-//      return $this->render('OCPlatformBundle:Advert:menu.html.twig', array(
-//        'listAdverts' => $listAdverts
-//      ));
-//    }
-//    
-    
+     public function viewUserAction()
+     {
+         return $this->render('ZDBlogBundle:Article:userPage.html.twig');
+     }
+     
 }
+  
